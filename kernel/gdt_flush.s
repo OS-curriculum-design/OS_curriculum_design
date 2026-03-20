24c00fba46ecfccc1853a4d41eda4c9d8eed4065
liuhaiyun <2183727347@qq.com>
2026-03-20 10:57:21 +0800

    增加注释

--- kernel/gdt_flush.s
+++ kernel/gdt_flush.s
@@ -3,17 +3,17 @@
 .global gdt_load
 
 gdt_load:
-    movl 4(%esp), %eax
-    lgdt (%eax)
+    movl 4(%esp), %eax//将参数传入eax
+    lgdt (%eax)//将eax中的值装入gdtr
 
-    movw $0x10, %ax
+    movw $0x10, %ax//内核数据段
     movw %ax, %ds
     movw %ax, %es
     movw %ax, %fs
     movw %ax, %gs
     movw %ax, %ss
 
-    ljmp $0x08, $1f
+    ljmp $0x08, $1f//内核指令段
 1:
     ret
 
